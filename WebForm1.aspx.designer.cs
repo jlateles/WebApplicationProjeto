3b90e8bed784c65e3a9406d7ec9a30e0d635d98a
Júlia Teles <juliatelesicm@gmail.com>
2023-11-14 12:00:06 -0300

    Update WebForm1.aspx.designer.cs

--- WebForm1.aspx.designer.cs
+++ WebForm1.aspx.designer.cs
@@ -51,15 +51,6 @@ namespace WebApplicationProjeto
         protected global::System.Web.UI.WebControls.GridView gdvExibir;
 
         /// <summary>
-        /// Controle SqlDataSource1.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
-
-        /// <summary>
         /// Controle btnEnviar.
         /// </summary>
         /// <remarks>
