e0d43bf00395dc681403d5b632f9e048d8e722c6
Júlia Teles <juliatelesicm@gmail.com>
2023-11-14 11:27:13 -0300

    Update Login.aspx.designer.cs

--- Login.aspx.designer.cs
+++ Login.aspx.designer.cs
@@ -42,6 +42,15 @@ namespace WebApplicationProjeto
         protected global::System.Web.UI.WebControls.GridView gdvExibir;
 
         /// <summary>
+        /// Controle btnLogin.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogin;
+
+        /// <summary>
         /// Controle lblMensagem.
         /// </summary>
         /// <remarks>
