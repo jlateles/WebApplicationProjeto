7cb0f9e90208e3be46d3f46232bad9353c8763d4
Júlia Teles <juliatelesicm@gmail.com>
2023-11-14 11:15:31 -0300

    Update Login.aspx.cs

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -7,11 +9,59 @@ using System.Web.UI.WebControls;
 
 namespace WebApplicationProjeto
 {
-    public partial class WebForm2 : System.Web.UI.Page
+    public partial class Login : System.Web.UI.Page
     {
+
+        /* inicio configuração SQL server */
+        SqlConnection conexao = new SqlConnection();
+        SqlCommand comandos = new SqlCommand();
+        SqlDataAdapter adaptadorSql = new SqlDataAdapter();
+        DataTable tabelaDados = new DataTable();
+        DataSet dataSet = new DataSet();
+        /* fim configuração SQL Server */
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        conexao.ConnectionString = "Data Source=51DE247989\\SENAC;Initial CatalogL = escola;Integrated Security=true";
+        conexao.Open();
+
         }
+
+        public void checarUsuario() {
+
+            dataSet = new DataSet();
+            comandos.CommandText = "INSERT INTO aluno(email, senha)VALUES(' " + txbEmail.Text.ToString() + " ', ' " + txbSenha.Text.ToString() + " ')";
+            comandos.Connection = conexao;
+            adaptadorSql = new SqlDataAdapter(comandos);
+            adaptadorSql.Fill(dataSet);
+            comandos.ExecuteNonQuery();
+            /* carregar o conteudo em um objeto */
+            SqlDataReader dados = comandos.ExecuteReader();
+            /* dados ["ide_aluno"], dados["email"], dados["senha"]*/
+
+            if (dados.HasRows)
+            {
+                dados.Read(); /*faz a leitura do que recebeuem dados */
+                string dadosEmail = dados["email"].ToString();
+                string dadosId = dados["id_aluno"].ToString();
+
+                /* cria um cookie com os dados de email do banco*/
+                HttpCookie cookieEmail = new HttpCookie("cookieEmail", dadosEmail);
+                ; Response.Cookies.Add(cookieEmail); /*adiciona o cookie a seção*/
+
+                /* cria um cookie com o id_aluno do banco*/
+                HttpCookie cookieId = new HttpCookie("cookieId", dadosId);
+                Response.Cookies.Add(cookieId);
+
+                Response.Redirect("");
+            }
+            else
+            {
+                lblMensagem.Text = "Email ou senha inválido(s)!";
+            }
+
+        }
+
+
     }
 }
