01b73370fedff4c5776765796775c87bd37a771a
Júlia Teles <juliatelesicm@gmail.com>
2023-11-16 09:40:28 -0300

    Create Cadastrados.aspx.cs

---
+++ View/Cadastrados.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationProjeto
+{
+    public partial class Cadastrados : System.Web.UI.Page
+    {
+
+        /* Inicio Configuração SQL Server */
+        SqlConnection conexcao = new SqlConnection();/* Criando a conexão */
+        SqlCommand comandos = new SqlCommand();/* Executará os comandos */
+        SqlDataAdapter adaptadorSql = new SqlDataAdapter(); /* Criação do Adaptador para Conexão */
+        DataTable tabelaDados = new DataTable(); /* Tabela contendo os Dados */
+        DataSet dataSet = new DataSet(); /* Objeto de interação com os dados */
+        /* Fim Configuração SQl Server */
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /* Chave do Connection Strig, Propriedade Cadeia de Caracteres*/
+            conexcao.ConnectionString = "Data Source=51DE247989\\SENAC;Initial Catalog=escola;Integrated Security=True";
+            conexcao.Open();/* Abertura da Conexão com o Connection String */
+
+            dataSet = new DataSet();/* Recebendo o virá da tabela*/
+            comandos.CommandText = " SELECT * FROM aluno ";
+            comandos.Connection = conexcao;
+            adaptadorSql = new SqlDataAdapter(comandos);
+            adaptadorSql.Fill(dataSet);
+            comandos.ExecuteNonQuery();
+            gdvExibir.DataSource = dataSet;
+            gdvExibir.DataBind();
+            conexcao.Close();
+
+        }
+    }
+}
